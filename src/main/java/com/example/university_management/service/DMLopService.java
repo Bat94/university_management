59ec6ac72fff95d03ba101ca44702f458127c72e
LAPTOP-T92SSGCT\Bui Tuan <tuanba.tgl@gmail.com>
2022-10-26 10:00:22 +0700

    update delete

--- src/main/java/com/example/university_management/service/DMLopService.java
+++ src/main/java/com/example/university_management/service/DMLopService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DMLopService {
 
@@ -22,5 +24,13 @@ public class DMLopService {
         dmLop.setHeDT(dmLopDto.getHeDT());
         dmLopRepository.save(dmLop);
         return ResponseEntity.ok(dmLop);
+    }
+
+    public ResponseEntity<List<DMLop>> getAll() {
+        List<DMLop> listAllDmLop = dmLopRepository.findAll();
+        if(listAllDmLop.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(listAllDmLop);
     }
 }
